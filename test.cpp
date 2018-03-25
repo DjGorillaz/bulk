27e416f502bef2673bdbc701af77099bf3bd01b6
DjGorillaz <djg2008@gmail.com>
2018-03-25 16:44:24 +0300

    Added tests

--- test.cpp
+++ test.cpp
@@ -9,14 +9,14 @@
 
 BOOST_AUTO_TEST_SUITE(Writing)
 
-bool testApp(const std::string& input, const std::string& output)
+bool testApp(const std::string& input, const std::string& output, int N)
 {
     std::ofstream test("testCase.log");
     test << input;
     test.close();
 
     std::ifstream test1("testCase.log");
-    App app(5, test1);
+    App app(N, test1);
 
     std::stringstream ss;
     app.exporters.emplace_back(std::make_unique<Console>(ss));
@@ -28,13 +28,34 @@ bool testApp(const std::string& input, const std::string& output)
     return ss.str() == output;
 }
 
-BOOST_AUTO_TEST_CASE(console)
+BOOST_AUTO_TEST_CASE(test1)
 {
-    std::string input = "1\n2\n3\n4\n5\n{\n1\n{\n{\n2\n}\n}\n3\n}\n1\n2\n";
-    std::string output = "bulk: 1, 2, 3, 4, 5\n"
-                        "bulk: 1, 2, 3\n"
-                        "bulk: 1, 2\n";
-    BOOST_REQUIRE(testApp(input, output));
+    std::string input = "1\n2\n3\n4\n5\n";
+    std::string output = "bulk: 1, 2, 3\n"
+                        "bulk: 4, 5\n";
+    BOOST_REQUIRE(testApp(input, output, 3));
+}
+
+BOOST_AUTO_TEST_CASE(test2)
+{
+    std::string input = "1\n2\n3\n{\n4\n5\n6\n7\n}\n";
+    std::string output = "bulk: 1, 2, 3\n"
+                        "bulk: 4, 5, 6, 7\n";
+    BOOST_REQUIRE(testApp(input, output, 3));
+}
+
+BOOST_AUTO_TEST_CASE(test3)
+{
+    std::string input = "{\n1\n2\n{\n3\n4\n}\n5\n6\n}\n";
+    std::string output = "bulk: 1, 2, 3, 4, 5, 6\n";
+    BOOST_REQUIRE(testApp(input, output, 3));
+}
+
+BOOST_AUTO_TEST_CASE(test4)
+{
+    std::string input = "1\n2\n3\n{\n4\n5\n6\n7\n";
+    std::string output = "bulk: 1, 2, 3\n";
+    BOOST_REQUIRE(testApp(input, output,3));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
